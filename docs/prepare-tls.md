c1cf1ebf503d8b28242458b60080b5dfcbdbe65c
Björn Svensson <bjorn.a.svensson@est.tech>
2021-05-07 22:18:11 +0200

    simple-server: prepare for using TLS connections

---
+++ docs/prepare-tls.md
@@ -0,0 +1,51 @@
+# Prepare for using TLS in Undertow server
+
+### Generating a self-signed certificate (keystore)
+keytool -genkey -alias ssl -keyalg RSA -keysize 2048 -dname "CN=simpleserver.est.tech,OU=IT,O=EST" -keystore server.keystore -storepass password -keypass password
+
+#### Inspect keystore
+keytool -list -keystore server.keystore
+
+#### Export the cert incl. public key
+keytool -exportcert -keystore server.keystore -alias ssl -storepass password -file server.cer
+
+### Create a truststore containing self-signed cert
+keytool -importcert -keystore server.truststore -alias ssl -storepass password -file server.cer -noprompt
+
+
+#### Move keystores to under src/main/resources/
+https://stackoverflow.com/questions/16374235/resources-and-config-loading-in-maven-project
+
+
+
+## Other info related to TLS
+
+### Export JKS to PKCS12
+keytool -importkeystore \
+    -srckeystore server.keystore \
+    -destkeystore server.keystore.p12 \
+    -deststoretype PKCS12 \
+    -srcalias ssl \
+    -deststorepass password \
+    -destkeypass password
+
+### Export p12 to pem
+#### Export cert
+openssl pkcs12 -in server.keystore.p12  -nokeys -out cert.pem
+#### Export private key
+openssl pkcs12 -in server.keystore.p12  -nodes -nocerts -out key.pem
+
+
+### Generating a trust store for trusted Certificate Authorities
+Default:
+keytool -list -keystore /usr/lib/jvm/java-11-openjdk-amd64/lib/security/cacerts
+
+(which points to /etc/ssl/certs/java/cacerts)
+
+
+### Optional: Generate with openssl and keytool
+https://unix.stackexchange.com/questions/347116/how-to-create-keystore-and-truststore-using-self-signed-certificate
+
+## Links
+http://seedstack.org/guides/https-with-undertow/
+https://www.baeldung.com/java-keystore-truststore-difference
--- simple-server/src/main/java/tech/est/simpleserver/App.java
+++ simple-server/src/main/java/tech/est/simpleserver/App.java
@@ -10,16 +10,39 @@ import io.undertow.util.Headers;
 import io.undertow.util.Protocols;
 
 import org.jboss.logging.Logger;
+import org.xnio.IoUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
 public class App {
     private static final Logger LOGGER = Logger.getLogger(App.class);
     private static final int PORT = 7777;
     private static final String HOST = "localhost";
+    private static final String SERVER_KEY_STORE = "server.keystore";
+    private static final String SERVER_TRUST_STORE = "server.truststore";
+    private static final char[] STORE_PASSWORD = "password".toCharArray();
 
     private Undertow server;
     private GracefulShutdownHandler shutdownHandler;
 
     public static void main(final String[] args) {
+
+        // NOT WORKING!?
+        // Enabling using: logging.properties
+        System.setProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager");
 
         App app = new App();
 
@@ -38,6 +61,13 @@ public class App {
     }
 
     public void createServer() {
+        SSLContext sslContext;
+        try {
+            sslContext = createSSLContext(loadKeyStore(SERVER_KEY_STORE), loadKeyStore(SERVER_TRUST_STORE));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         HttpHandler requestHandler = new HttpHandler() {
             @Override
             public void handleRequest(final HttpServerExchange exchange) throws Exception {
@@ -59,6 +89,12 @@ public class App {
             .addHttpListener(PORT, HOST)
             .setHandler(shutdownHandler).build();
 
+        // // h2 with TLS/SSL only
+        // server = Undertow.builder().setServerOption(UndertowOptions.ENABLE_HTTP2,
+        // true)
+        // .addHttpsListener(PORT, HOST,
+        // sslContext).setHandler(shutdownHandler).build();
+
         server.start();
     }
 
@@ -76,6 +112,63 @@ public class App {
         }
         LOGGER.info(">>> CALL await shutdown - DONE");
 
+        LOGGER.info(">>> CALL stop server");
         server.stop();
+    }
+
+    private static KeyStore loadKeyStore(final String name) throws IOException {
+        final InputStream stream = App.class.getClassLoader().getResourceAsStream(name);
+        if (stream == null) {
+            throw new RuntimeException("Could not load keystore: " + name);
+        }
+
+        try {
+            KeyStore loadedKeystore = KeyStore.getInstance("JKS");
+            loadedKeystore.load(stream, STORE_PASSWORD);
+            return loadedKeystore;
+
+        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
+            throw new IOException(String.format("Unable to load KeyStore %s", name), e);
+        } finally {
+            IoUtils.safeClose(stream);
+        }
+    }
+
+    private static SSLContext createSSLContext(final KeyStore keyStore, final KeyStore trustStore) throws IOException {
+        KeyManager[] keyManagers;
+        try {
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory
+                    .getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, STORE_PASSWORD);
+            keyManagers = keyManagerFactory.getKeyManagers();
+        } catch (NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException e) {
+            throw new IOException("Unable to initialise KeyManager[]", e);
+        }
+
+        TrustManager[] trustManagers;
+        try {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory
+                    .getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(trustStore);
+            trustManagers = trustManagerFactory.getTrustManagers();
+        } catch (NoSuchAlgorithmException | KeyStoreException e) {
+            throw new IOException("Unable to initialise TrustManager[]", e);
+        }
+
+        SSLContext sslContext;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagers, trustManagers, null);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new IOException("Unable to create and initialise the SSLContext", e);
+        }
+        // Use SNI?
+        // SNIContextMatcher matcher = new
+        // SNIContextMatcher.Builder().setDefaultContext(sslContext)
+        // .addMatch("localhost", sslContext).build();
+        // return new SNISSLContext(matche
+        // );
+
+        return sslContext;
     }
 }
